98827657cae9f1ea74277ea0e2e7234cad60fcfe
Charles Dolloso <Charles.Dolloso@practicemax.com>
2021-04-17 15:13:29 +0800

    commit upgrade angular to 11

--- src/app/exam/exam-form/exam-form.component.spec.ts
+++ src/app/exam/exam-form/exam-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ExamFormComponent } from './exam-form.component';
 
@@ -6,7 +6,7 @@ describe('ExamFormComponent', () => {
   let component: ExamFormComponent;
   let fixture: ComponentFixture<ExamFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExamFormComponent ]
     })
